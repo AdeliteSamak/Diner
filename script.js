62acf66c6cd96e8ad7e0f2063c3b6ee31f2eadc9
adelite <adelasamakova@gmail.com>
2020-02-05 23:39:36 +0100

    first dish

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ console.log(aCopy)
 
 aCopy.querySelector("h2").textContent = "Russian Ringbread";
 aCopy.querySelector("p:nth-child(2)").textContent = "Starter";
-aCopy.querySelector("p:nth-child(3)").textContent = "Russisk ringbrød af Karapatisk mel";
-aCopy.querySelector("p:nth-child(4)").textContent = "29 DKK";
+aCopy.querySelector("p:nth-child(5)").textContent = "29 DKK";
 
 const whosYourDaddy = document.querySelector("main");
 
 whosYourDaddy.appendChild(aCopy)
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
